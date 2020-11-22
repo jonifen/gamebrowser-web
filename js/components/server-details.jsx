8d1cefb660caa7eb06240bdc73324711886e1966
Jon Cain <jonifen@users.noreply.github.com>
2020-11-22 15:41:59 +0000

    Hard-code server response if localhost (faster feedback\!)

--- js/components/server-details.jsx
+++ js/components/server-details.jsx
@@ -16,6 +16,12 @@ function ServerDetails(server) {
   }
 
   const queryServer = function() {
+    if (server.ipAddress === "127.0.0.1") {
+      const dummyData = {"allDetails":{"sv_maxPing":"0","sv_minPing":"0","sv_punkbuster":"0","sv_floodProtect":"1","sv_maxRate":"0","sv_hostname":"noname","dmflags":"0","fraglimit":"20","timelimit":"0","sv_maxclients":"8","g_maxGameClients":"0","capturelimit":"0","version":"Q3 1.32c win-x86 May  8 2006","g_gametype":"0","protocol":"68","mapname":"Q3DM1","sv_privateClients":"0","sv_allowDownload":"0","bot_minplayers":"0","gamename":"baseq3","g_needpass":"0"},"gameName":"baseq3","gameType":"0","ipAddress":"127.0.0.1","mapName":"Q3DM1","maxClients":8,"name":"noname","players":[{"name":"7jon1.","ping":0,"score":0}, {"name":"^2greenmonster","ping":0,"score":0}],"ping":0,"port":27960,"status":0};
+      setServerDetails(dummyData);
+      return;
+    }
+
     getServerDetails(server.ipAddress, server.port, server.game)
       .then(data => {
         setServerDetails(data);
